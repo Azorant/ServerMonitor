7cc519eed7140728d1617628ee2d175d0e176953
Derek Alsop <15299183+Azorant@users.noreply.github.com>
2025-05-10 03:47:46 -0700

    Initial commit

---
+++ Template.Bot/Jobs/StatusJob.cs
@@ -0,0 +1,29 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Template.Bot.Services;
+
+namespace Template.Bot.Jobs;
+
+public class StatusJob(IServiceProvider serviceProvider)
+{
+    private int lastStatus;
+    private readonly string[] statuses = ["/help", "eris.gg"];
+
+    public async Task SetStatus()
+    {
+        try
+        {
+            var client = serviceProvider.GetRequiredService<DiscordSocketClient>();
+            var prom = serviceProvider.GetRequiredService<PrometheusService>();
+            await client.SetCustomStatusAsync(statuses[lastStatus]);
+            lastStatus++;
+            if (lastStatus == statuses.Length) lastStatus = 0;
+            prom.Latency.Set(client.Latency);
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Failed to set status");
+        }
+    }
+}
---
+++ Template.Bot/Services/PrometheusService.cs
@@ -0,0 +1,10 @@
+﻿using Prometheus;
+
+namespace Template.Bot.Services;
+
+public class PrometheusService
+{
+    public Gauge Guilds { get; set; } = Metrics.CreateGauge("guilds", "How many guilds the bot is in");
+    public Counter Commands { get; set; } = Metrics.CreateCounter("commands_total", "Commands ran", labelNames: ["command"]);
+    public Gauge Latency { get; set; } = Metrics.CreateGauge("latency", "Websocket latency");
+}
